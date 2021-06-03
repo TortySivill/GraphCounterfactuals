7236f15dd6fb5cc3ffae8b894adeac32435d33eb
tombewley <tom.bewley@bristol.ac.uk>
2021-06-03 12:30:45 +0100

    Pull UDM code into a class

---
+++ Tom/test_UDM.py
@@ -0,0 +1,19 @@
+from UDM import UDM
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("Tom/table3.csv")
+X = df.values
+ordinal = ["O" in c for c in df.columns]
+
+udm = UDM(X, ordinal)
+
+mask = np.ones((X.shape[0], X.shape[0]), dtype=bool)
+mask[2,0] = 0
+mask[0,2] = 0
+mask[1,3] = 0
+
+print(udm(X, mask=mask, placeholder=np.inf))
+
+print(udm(X[2], X[3]))
